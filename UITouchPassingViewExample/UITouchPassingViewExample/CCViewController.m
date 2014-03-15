dd711331138bad351b69de28748cb1f0114759bd
Carlin <email.me@carlinyuen.com>
2014-03-15 12:34:16 -0400

    fleshing out project

--- UITouchPassingViewExample/UITouchPassingViewExample/CCViewController.m
+++ UITouchPassingViewExample/UITouchPassingViewExample/CCViewController.m
@@ -10,6 +10,8 @@
 
 @interface CCViewController ()
 
+    @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
 @end
 
 @implementation CCViewController
@@ -18,6 +20,30 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    self.scrollView.clipsToBounds = false;
+    self.scrollView.pagingEnabled = true;
+    self.scrollView.directionalLockEnabled = true;
+    self.scrollView.showsHorizontalScrollIndicator = true;
+    self.scrollView.showsVerticalScrollIndicator =  false;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+    // Setup frames
+    CGRect bounds = self.scrollView.bounds;
+
+    // Basic populate scroll view with random views
+    for (NSInteger i = 0; i < 10; i++) {
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(
+            CGRectGetWidth(bounds) * i + 4, 4,
+            CGRectGetWidth(bounds) - 8,
+            CGRectGetHeight(bounds) - 8
+        )];
+        view.backgroundColor = [UIColor greenColor];
+    }
 }
 
 - (void)didReceiveMemoryWarning
